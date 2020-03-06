42c2c0923b7f25a528cae058b57edf4319df093f
henrique-temponi <tanzenheavely@gmail.com>
2020-03-05 23:36:00 -0300

    inicia o primeiro programa
    Descreve melhor a funcao do programa
    adiciona funcoes basicas

---
+++ textToGraph.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Text to Graph program,
+ * this is meant to be a simple program that read a given number of inputs in stdin.
+ * the structure will follow the pattern:
+ * First line will give the total number of nodes.
+ * when the following lines will give two 2 nodes, meaning that two nodes have a edge.
+ * the final input will be a single line with END.
+ * 
+ * After receiving the inputs, the program will generate a graph using a matrix.
+ */
+public class textToGraph {
+
+    public static String input(BufferedReader reader) {
+        String input = "";
+        
+        try {
+            input = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return input;
+    }
+
+    public static void main(String[] args) {
+        
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
